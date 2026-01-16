0f50d24df11fdb58696b6843bab03a37888452dd
Lisa <lisadsouza102@gmail.com>
2026-01-15 18:19:05 -0700

    Lab 2 demo

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listycity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    //declare the variables so you will be able to reference it later
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow","Sydney","Berlin","Vienna","Tokyo","Beijing","Osaka","New Delhi"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+    }
+}
