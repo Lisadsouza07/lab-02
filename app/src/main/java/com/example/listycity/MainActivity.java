36d6cf2349809b90325bd37df9e6d23eac050064
Lisa <lisadsouza102@gmail.com>
2026-01-16 11:12:58 -0700

    Implemented Lab2 final-v

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.listycity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,19 +20,70 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
+    Button btnAdd;
+    Button btnConfirm;
+    Button btnDelete;
+    EditText editCity;
+
+    int selectedPosition = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         cityList = findViewById(R.id.city_list);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnConfirm = findViewById(R.id.btnConfirm);
+        btnDelete = findViewById(R.id.btnDelete);
+        editCity = findViewById(R.id.editCity);
 
-        String []cities = {"Edmonton", "Vancouver", "Moscow","Sydney","Berlin","Vienna","Tokyo","Beijing","Osaka","New Delhi"};
+        String[] cities = {"Edmonton", "Vancouver"};
 
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
 
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, dataList);
         cityList.setAdapter(cityAdapter);
+
+        //select city
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+            cityList.setItemChecked(position, true);
+        });
+
+        // ADD CITY → focus input
+        btnAdd.setOnClickListener(v -> editCity.requestFocus());
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = editCity.getText().toString().trim();
+
+                if (!city.isEmpty()) {
+                    dataList.add(city);
+                    cityAdapter.notifyDataSetChanged();
+                    editCity.setText("");
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter a city",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
+        btnDelete.setOnClickListener(v -> {
+            if (selectedPosition != -1) {
+                dataList.remove(selectedPosition);
+                cityAdapter.notifyDataSetChanged();
+                selectedPosition = -1;
+            } else {
+                Toast.makeText(this, "Select city to Delete", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
+
+
